5a46fe0d868ac56854cab8fd58e8ec49d7397abb
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-12-20 00:00:10 -0500

    move function

--- src/api.js
+++ src/api.js
@@ -1,5 +1,24 @@
 import mockData from './mock-data';
 
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+export const extractLocations = (events) => {
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
+};
+
+/**
+ *
+ * This function will fetch the list of all events
+ */
+
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
@@ -21,24 +40,6 @@ export const getAccessToken = async () => {
   return accessToken;
 
 };
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
-export const extractLocations = (events) => {
-  const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
-  return locations;
-};
-
-/**
- *
- * This function will fetch the list of all events
- */
 
 const checkToken = async (accessToken) => {
   const response = await fetch(
