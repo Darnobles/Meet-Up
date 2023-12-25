01819fe4f6a6332dc5112f489962dc74547aee4e
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-12-25 17:41:39 -0500

    change value in type and dataKey in the x and y axis of the scatterChart

--- src/setupTests.js
+++ src/setupTests.js
@@ -18,3 +18,20 @@ if (!ignoreMessage) originalError(...args);
 }
 
 jest.setTimeout(40000);
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
